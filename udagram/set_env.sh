986b1574402b1e61005783ae75f7e08419bc7610
unknown <eman.hamdy666@yahoo.com>
2022-11-05 12:47:27 +0200

    upload

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-2.cdz7fuh5fkzi.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_MEDIA_BUCKET=front-udagram
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=udagram.eba-5nmdprtm.us-east-1.elasticbeanstalk.com/
